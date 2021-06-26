faf58095354fa984a13345185df6042b7150949a
SUTHARRAM <rls42847@gmail.com>
2021-06-26 23:54:55 +0530

    mi

--- my_store/settings.py
+++ my_store/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '6##3le!yt^p8w827oyd+cn$&*c6bn30s6)sz&_ot@v4d^#u=vb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
